3f8a97ed029da346431cd560707d542ec300de28
dfrugo <dariusz.slomka88@gmail.com>
2020-07-05 14:41:49 +0200

    light/dark mode button

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import '../scss/main.scss';
 
-import * as moment from 'moment'
+import * as moment from 'moment';
+
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
 // registerSW();
@@ -10,3 +11,25 @@ import * as moment from 'moment'
 console.log('HELLO 🚀')
 let time = moment().startOf('day').fromNow();
 console.log(time);
+
+let isDark = true;
+let modeButton = document.querySelector('.mode-button--js');
+modeButton.addEventListener('click', () => {
+    if(isDark)
+    {
+        document.documentElement.style.setProperty('--background-color', 'whitesmoke');
+        document.documentElement.style.setProperty('--font-color', '#222629');
+        console.log('lightMode -> on')
+    }
+    else
+    {
+        document.documentElement.style.setProperty('--background-color', '#222629');
+        document.documentElement.style.setProperty('--font-color', 'whitesmoke');
+        console.log('darkMode -> on')
+    }
+
+    isDark = !isDark;
+})
+
+
+
